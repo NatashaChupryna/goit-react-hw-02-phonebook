4acd51a58d863af370daadb593ef3421ac712044
Natasha <natkamonte1992@gmail.com>
2023-01-16 16:57:12 +0200

    filter method

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const StyledApp = styled.div`
+  height: 100vh;
+  /* display: flex;
+  justifycontent: center;
+  alignitems: center; */
+  fontsize: 40;
+  color: #010101;
+  padding-top: 30px;
+  padding-left: 30px;
+`;
